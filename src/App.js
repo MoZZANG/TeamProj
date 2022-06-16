79e40bd05931970c8fd4d919b02d8f0f14711541
MoZZANG <98200337+MoZZANG@users.noreply.github.com>
2022-06-16 10:58:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
+import { useEffect, useRef, useState } from "react";
+import WholeMap from "./components/WholeMap/WholeMap";
+
+import MainPage from "./components/MainPage/MainPage";
+import Login from "./components/Login/Login";
 
 function App() {
+  let scroll = useRef();
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const HandleScroll = () => {
+    if (window.scrollY > 990 && window.scrollY < 1780) setScrollNav(true);
+    else setScrollNav(false);
+  };
+  window.addEventListener("scroll", HandleScroll);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route element={<Navbar scrollNav={scrollNav} />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/WholeMap" element={<WholeMap />}></Route>
+          <Route path="/mainPage/:currPosition" element={<MainPage />}></Route>
+        </Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
