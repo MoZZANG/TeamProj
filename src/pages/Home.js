d352140c3bba7b5bf4072e5c0eb4fe2d2fd523fa
MoZZANG <98200337+MoZZANG@users.noreply.github.com>
2022-06-16 11:00:14 +0900

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Cards from "../components/Cards/Cards.js";
+import IntroPart from "../components/IntroPart/IntroPart.js";
+import Swipers from "../components/Swipers/Swipers.js";
+const Home = () => {
+  return (
+    <>
+      <IntroPart />
+      <Swipers />
+      <Cards />
+    </>
+  );
+};
+
+export default Home;
