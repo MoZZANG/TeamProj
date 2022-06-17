2ff0b8b6af9735fdf91bfb91fa2b5255e94504ad
MoZZANG <98200337+MoZZANG@users.noreply.github.com>
2022-06-17 14:29:36 +0900

    Create RightRecommandSide.js

---
+++ src/components/RightRecommandSide/RightRecommandSide.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./RightRecommandSide.css";
+const RightRecommandSide = () => {
+  return (
+    <div className="RightRecommandSide">
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+      <div>RightRecommandSide</div>
+    </div>
+  );
+};
+
+export default RightRecommandSide;
