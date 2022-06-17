6e759e1c109fef81db9b694e7fe1999884ec9a9a
MoZZANG <98200337+MoZZANG@users.noreply.github.com>
2022-06-17 14:29:21 +0900

    Update MainPage.js

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./MainPage.css";
 import KMap from "../KMap/KMap.js";
 import { useParams } from "react-router-dom";
+import LeftPlanSide from "../LeftPlanSide/LeftPlanSide";
+import RightRecommandSide from "../RightRecommandSide/RightRecommandSide";
 const MainPage = () => {
   let { currPosition } = useParams();
   return (
-    <div>
+    <div className="MainPage">
+      <LeftPlanSide currPosition={currPosition} />
       <KMap currPosition={currPosition} />
+      <RightRecommandSide />
     </div>
   );
 };
