83dde7ed8740939ed439c1732626addf48a3712d
kim ki moon <98200337+MoZZANG@users.noreply.github.com>
2022-06-20 21:20:25 +0900

    Update WholeMapLocalData.js

--- src/components/WholeMap/WholeMapLocalData.js
+++ src/components/WholeMap/WholeMapLocalData.js
@@ -1,13 +1,13 @@
 let WholeMapLocalData = [
   { id: "gangwon", localName: "강원도" },
-  { id: "seoul", localName: "서울특별시" },
+  { id: "seoul", localName: "서울" },
   { id: "geonggi", localName: "경기도" },
   { id: "incheon", localName: "인천" },
   { id: "chungbuk", localName: "충청북도" },
   { id: "chungnam", localName: "충청남도" },
   { id: "busan", localName: "부산" },
-  { id: "daegu", localName: "대구광역시" },
-  { id: "daejeon", localName: "대전광역시" },
+  { id: "daegu", localName: "대구" },
+  { id: "daejeon", localName: "대전" },
   { id: "gwangju", localName: "광주광역시" },
   { id: "gyeongbuk", localName: "경상북도" },
   { id: "gyeongnam", localName: "경상남도" },
@@ -15,6 +15,8 @@ let WholeMapLocalData = [
   { id: "jeonbuk", localName: "전라북도" },
   { id: "jeonnam", localName: "전라남도" },
   { id: "ulsan", localName: "울산" },
+  //test용 hard-data
+  // { id: "chuncheon", localName: "춘천" },
 ];
 
 export default WholeMapLocalData;
