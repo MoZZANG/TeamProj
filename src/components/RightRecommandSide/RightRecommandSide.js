c3d147d4644b562fe412668309539744786183e8
kim ki moon <98200337+MoZZANG@users.noreply.github.com>
2022-06-20 21:20:15 +0900

    Update RightRecommandSide.js

--- src/components/RightRecommandSide/RightRecommandSide.js
+++ src/components/RightRecommandSide/RightRecommandSide.js
@@ -1,11 +1,68 @@
+import { faBed, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Search from "../Search/Search";
+import SearchedLocation from "../SearchedLocation/SearchedLocation";
 import "./RightRecommandSide.css";
 const RightRecommandSide = () => {
+  //searchedLocaion 테스트용 배열
+  let arr = ["경복궁", "창덕궁", "남산타워"];
+
   return (
     <div className="RightRecommandSide">
-      <Search />
-      <SearchedLocation />
+      <div>
+        <Search />
+      </div>
+      <div className="rightRecommandSide__span__container">
+        <span
+          className="rightRecommandSide__span"
+          onClick={(e) => {
+            e.stopPropagation();
+            e.target.classList.toggle("rRS_span-active");
+            e.target.nextElementSibling.classList.toggle("rRS_span-active");
+          }}>
+          <FontAwesomeIcon
+            icon={faBed}
+            className="rightRecommandSide__span-i"
+          />
+          호텔
+        </span>
+        <span
+          className="rightRecommandSide__span rRS_span-active"
+          onClick={(e) => {
+            e.stopPropagation();
+            e.target.classList.toggle("rRS_span-active");
+            e.target.previousElementSibling.classList.toggle("rRS_span-active");
+          }}>
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            className="rightRecommandSide__span-i"
+          />
+          장소
+        </span>
+      </div>
+
+      <div className="rightRecommandSide__searchedLocation__container">
+        <h3>{`검색결과(?건)`}</h3>
+        {arr.map((local, index) => {
+          return <SearchedLocation key={index} local={local} />;
+        })}
+        {/* <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation />
+        <SearchedLocation /> */}
+      </div>
     </div>
   );
 };
