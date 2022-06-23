dc62e9aa731a18d0749d05456f55d71797749625
kim ki moon <98200337+MoZZANG@users.noreply.github.com>
2022-06-23 17:21:58 +0900

    Update MainPage.js

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainPage.css";
 import KMap from "../KMap/KMap.js";
 import { useParams } from "react-router-dom";
 import LeftPlanSide from "../LeftPlanSide/LeftPlanSide";
 import RightRecommandSide from "../RightRecommandSide/RightRecommandSide";
+import Navbar from "../Navbar/Navbar";
 const MainPage = () => {
+  const [titleName, setTitleName] = useState("");
   let { currPosition } = useParams();
   return (
     <div className="MainPage">
       <LeftPlanSide currPosition={currPosition} />
-      <KMap currPosition={currPosition} />
-      <RightRecommandSide />
+      <KMap currPosition={currPosition} setTitleName={setTitleName} />
+      <RightRecommandSide setTitleName={setTitleName} titleName={titleName} />
     </div>
   );
 };
