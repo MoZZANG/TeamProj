8f2cda8faea88a4df47177edcada8651cb723fbd
kim ki moon <98200337+MoZZANG@users.noreply.github.com>
2022-06-27 17:37:30 +0900

    Update MainPage.js

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -6,12 +6,21 @@ import LeftPlanSide from "../../components/LeftPlanSide/LeftPlanSide";
 import RightRecommandSide from "../../components/RightRecommandSide/RightRecommandSide";
 const MainPage = () => {
   const [titleName, setTitleName] = useState("");
+  const [conWhichModal, setConWhichModal] = useState(false);
   let { currPosition } = useParams();
   return (
     <div className="MainPage">
       <LeftPlanSide currPosition={currPosition} />
-      <KMap currPosition={currPosition} setTitleName={setTitleName} />
-      <RightRecommandSide setTitleName={setTitleName} titleName={titleName} />
+      <KMap
+        currPosition={currPosition}
+        setTitleName={setTitleName}
+        setConWhichModal={setConWhichModal}
+      />
+      <RightRecommandSide
+        setTitleName={setTitleName}
+        titleName={titleName}
+        conWhichModal={conWhichModal}
+      />
     </div>
   );
 };
